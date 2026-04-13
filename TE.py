7af07fa15bbaf910b5fc3e05a0c21f3ddafd6fac
gabbo96 <gabriele.barile96@gmail.com>
2026-04-13 13:53:49 +0200

    Formatting

--- TE.py
+++ TE.py
@@ -2,114 +2,106 @@
 Transfer Entropy -- https://github.com/notsebastiano/transfer_entropy
 
 Contributor:
-	 Sebastiano Bontorin (sbontorin@fbk.eu)
+         Sebastiano Bontorin (sbontorin@fbk.eu)
 """
 
 import numpy as np
 from scipy import stats
 from scipy import ndimage
 
-def transfer_entropy(X,Y,delay=1,gaussian_sigma=None):
-	'''
-	TE implementation: asymmetric statistic measuring the reduction in uncertainty
-	for a future value of X given the history of X and Y.
-	Calculated through the Kullback-Leibler divergence with conditional probabilities
 
-	Quantifies the amount of information from Y to X.
+def transfer_entropy(X, Y, delay=1, gaussian_sigma=None):
+    """
+    TE implementation: asymmetric statistic measuring the reduction in uncertainty
+    for a future value of X given the history of X and Y.
+    Calculated through the Kullback-Leibler divergence with conditional probabilities
 
-	author: Sebastiano Bontorin
-	mail: sbontorin@fbk.eu
+    Quantifies the amount of information from Y to X.
 
-	args:
-		- X (1D array):
-			time series of scalars (1D array)
-		- Y (1D array):
-			time series of scalars (1D array)
-	kwargs:
-		- delay (int): 
-			step in tuple (x_n, y_{n - delay}, x_(n - delay))
-		- gaussian_sigma (int):
-			sigma to be used
-			default set at None: no gaussian filtering applied
-	returns:
-		- TE (float):
-			transfer entropy between X and Y given the history of X
-	'''
+    author: Sebastiano Bontorin
+    mail: sbontorin@fbk.eu
 
-	if len(X)!=len(Y):
-		raise ValueError('time series entries need to have same length')
+    args:
+            - X (1D array):
+                    time series of scalars (1D array)
+            - Y (1D array):
+                    time series of scalars (1D array)
+    kwargs:
+            - delay (int):
+                    step in tuple (x_n, y_{n - delay}, x_(n - delay))
+            - gaussian_sigma (int):
+                    sigma to be used
+                    default set at None: no gaussian filtering applied
+    returns:
+            - TE (float):
+                    transfer entropy between X and Y given the history of X
+    """
 
-	n = float(len(X[delay:]))
+    if len(X) != len(Y):
+        raise ValueError("time series entries need to have same length")
 
-	# number of bins for X and Y using Freeman-Diaconis rule
-	# histograms built with numpy.histogramdd
-	binX = int( (max(X)-min(X))
-				/ (2* stats.iqr(X) / (len(X)**(1.0/3))) )
-	binY = int( (max(Y)-min(Y))
-				/ (2* stats.iqr(Y) / (len(Y)**(1.0/3))) )
+    n = float(len(X[delay:]))
 
-	# Definition of arrays of shape (D,N) to be transposed in histogramdd()
-	x3 = np.array([X[delay:],Y[:-delay],X[:-delay]])
-	x2 = np.array([X[delay:],Y[:-delay]])
-	x2_delay = np.array([X[delay:],X[:-delay]])
+    # number of bins for X and Y using Freeman-Diaconis rule
+    # histograms built with numpy.histogramdd
+    binX = int((max(X) - min(X)) / (2 * stats.iqr(X) / (len(X) ** (1.0 / 3))))
+    binY = int((max(Y) - min(Y)) / (2 * stats.iqr(Y) / (len(Y) ** (1.0 / 3))))
 
-	p3,bin_p3 = np.histogramdd(
-		sample = x3.T,
-		bins = [binX,binY,binX])
+    # Definition of arrays of shape (D,N) to be transposed in histogramdd()
+    x3 = np.array([X[delay:], Y[:-delay], X[:-delay]])
+    x2 = np.array([X[delay:], Y[:-delay]])
+    x2_delay = np.array([X[delay:], X[:-delay]])
 
-	p2,bin_p2 = np.histogramdd(
-		sample = x2.T,
-		bins=[binX,binY])
+    p3, bin_p3 = np.histogramdd(sample=x3.T, bins=[binX, binY, binX])
 
-	p2delay,bin_p2delay = np.histogramdd(
-		sample = x2_delay.T,
-		bins=[binX,binX])
+    p2, bin_p2 = np.histogramdd(sample=x2.T, bins=[binX, binY])
 
-	p1,bin_p1 = np.histogramdd(
-		sample = np.array(X[delay:]),
-		bins=binX)
+    p2delay, bin_p2delay = np.histogramdd(sample=x2_delay.T, bins=[binX, binX])
 
-	# Hists normalized to obtain densities
-	p1 = p1/n
-	p2 = p2/n
-	p2delay = p2delay/n
-	p3 = p3/n
+    p1, bin_p1 = np.histogramdd(sample=np.array(X[delay:]), bins=binX)
 
-	# If True apply gaussian filters at given sigma to the distributions
-	if gaussian_sigma is not None:
-		s = gaussian_sigma
-		p1 = ndimage.gaussian_filter(p1, sigma=s)
-		p2 = ndimage.gaussian_filter(p2, sigma=s)
-		p2delay = ndimage.gaussian_filter(p2delay, sigma=s)
-		p3 = ndimage.gaussian_filter(p3, sigma=s)
+    # Hists normalized to obtain densities
+    p1 = p1 / n
+    p2 = p2 / n
+    p2delay = p2delay / n
+    p3 = p3 / n
 
-	# Ranges of values in time series
-	Xrange = bin_p3[0][:-1]
-	Yrange = bin_p3[1][:-1]
-	X2range = bin_p3[2][:-1]
+    # If True apply gaussian filters at given sigma to the distributions
+    if gaussian_sigma is not None:
+        s = gaussian_sigma
+        p1 = ndimage.gaussian_filter(p1, sigma=s)
+        p2 = ndimage.gaussian_filter(p2, sigma=s)
+        p2delay = ndimage.gaussian_filter(p2delay, sigma=s)
+        p3 = ndimage.gaussian_filter(p3, sigma=s)
 
-	# Calculating elements in TE summation
-	elements = []
-	for i in range(len(Xrange)):
-		px = p1[i]
-		for j in range(len(Yrange)):
-			pxy = p2[i][j]
+    # Ranges of values in time series
+    Xrange = bin_p3[0][:-1]
+    Yrange = bin_p3[1][:-1]
+    X2range = bin_p3[2][:-1]
 
-			for k in range(len(X2range)):
-				pxx2 = p2delay[i][k]
-				pxyx2 = p3[i][j][k]
+    # Calculating elements in TE summation
+    elements = []
+    for i in range(len(Xrange)):
+        px = p1[i]
+        for j in range(len(Yrange)):
+            pxy = p2[i][j]
 
-				arg1 = float(pxy*pxx2)
-				arg2 = float(pxyx2*px)
+            for k in range(len(X2range)):
+                pxx2 = p2delay[i][k]
+                pxyx2 = p3[i][j][k]
 
-				# Corrections avoding log(0)
-				if arg1 == 0.0: arg1 = float(1e-8)
-				if arg2 == 0.0: arg2 = float(1e-8)
+                arg1 = float(pxy * pxx2)
+                arg2 = float(pxyx2 * px)
 
-				term = pxyx2*np.log2(arg2) - pxyx2*np.log2(arg1) 
-				elements.append(term)
+                # Corrections avoding log(0)
+                if arg1 == 0.0:
+                    arg1 = float(1e-8)
+                if arg2 == 0.0:
+                    arg2 = float(1e-8)
 
-	# Transfer Entropy
-	TE = np.sum(elements)
-	return TE
+                term = pxyx2 * np.log2(arg2) - pxyx2 * np.log2(arg1)
+                elements.append(term)
 
+    # Transfer Entropy
+    TE = np.sum(elements)
+    return TE
